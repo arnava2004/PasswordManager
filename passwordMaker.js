fd9474bf6f2825f18d99f8c4851420779db9143b
Tejes Prabakaran <tejessrihari.p@gmail.com>
2023-12-22 01:27:44 -0500

    I linked the slider and number count for password length.

--- passwordMaker.js
+++ passwordMaker.js
@@ -3,7 +3,17 @@ const specialChars = ["!", "?", "@", "#", "$", "%", "^", "&", "*","-", "_", "/"]
 const lowerLetters = [...Array(26)].map((_,i) => String.fromCharCode(i + 97));
 const upperLetters = lowerLetters.map(letter => letter.toUpperCase());
 
+const charAmountRange = document.getElementById("charAmountRange");
+const charAmountNum = document.getElementById("charAmountNum");
 
+charAmountRange.addEventListener("input", syncLength);
+charAmountNum.addEventListener("input", syncLength);
+
+function syncLength(e) {
+    const value = e.target.value;
+    charAmountRange.value = value;
+    charAmountNum.value = value;
+}
 // Will make range input later, make it changeable
 const passwordLength = 15
 
