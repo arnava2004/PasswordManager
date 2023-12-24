2a1677d5ffa4d55e19250d083cd80da6872ba682
Tejes Prabakaran <tejessrihari.p@gmail.com>
2023-12-23 23:47:51 -0500

    I am creating the API for the passwordChecker.

---
+++ BackEnd/implementing/Checker.py
@@ -0,0 +1,37 @@
+from flask import Flask
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer 
+
+Checker = Flask(__name__)
+
+@Checker.route('/hello/', methods=['GET', 'POST'])
+def welcome():
+    return "Hello World!"
+
+@Checker.route('/model/', methods=['GET', 'POST'])
+def display():
+    def tokens(string):
+        """Break strings into characters."""
+        return [x for x in string]
+    
+    print("libraries imported")
+
+    df = pd.read_csv("/Users/tejes/Chrome-exten/PasswordManager/BackEnd/data.csv", on_bad_lines='skip')
+    df=df.dropna()
+    X=df.drop("strength",axis=1).values.flatten()
+    y=df["strength"].values
+    print("data processed")
+
+    with open('/Users/tejes/Chrome-exten/PasswordManager/BackEnd/password_model.pkl', 'rb') as file:
+        model = pickle.load(file)
+
+    print(model.score(X,y))
+
+if __name__ == '__main__':
+    Checker.run(host='0.0.0.0', port=105)
+
--- BackEnd/passwordChecker.py
+++ BackEnd/passwordChecker.py
@@ -1,6 +1,9 @@
 # import libraries
+
 import pickle
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer 
@@ -13,7 +16,7 @@ print("libraries imported")
 
 # read and process the data
 
-df = pd.read_csv("/Users/arnav/Developer/Personal Projects/PasswordManager/BackEnd/data.csv", on_bad_lines='skip')
+df = pd.read_csv("/Users/tejes/Chrome-exten/PasswordManager/BackEnd/data.csv", on_bad_lines='skip')
 df=df.dropna()
 X=df.drop("strength",axis=1).values.flatten()
 y=df["strength"].values
@@ -21,7 +24,7 @@ print("data processed")
 
 # Machine learning pipeline
 
-password_clf=Pipeline([("vect", TfidfVectorizer(tokenizer=tokens)), ("clf",DecisionTreeClassifier())])
+password_clf=Pipeline([("vect",TfidfVectorizer(tokenizer=tokens)),("clf",DecisionTreeClassifier())])
 print("model training in progress")
 password_clf.fit(X,y)
 print("model is trained")
@@ -29,7 +32,7 @@ print(password_clf.score(X,y))
 
 # Save the trained model pipeline to a file
 
-with open('password_model.pkl', 'wb') as model_file:
-    pickle.dump(password_clf, model_file)
+#with open('password_model.pkl', 'wb') as model_file:
+#    pickle.dump(password_clf, model_file)
 
 print("pipeline saved")
