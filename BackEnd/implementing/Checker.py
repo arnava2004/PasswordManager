aec7ebe75b2f5462f57c7d835740947779504efe
Tejes Prabakaran <tejessrihari.p@gmail.com>
2023-12-23 23:57:33 -0500

    I modified code.

--- BackEnd/implementing/Checker.py
+++ BackEnd/implementing/Checker.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 import pickle
 import pandas as pd
 import numpy as np
@@ -6,6 +7,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer 
+
+def tokens(string):
+    """Break strings into characters."""
+    return [x for x in string]
+    
+print("libraries imported")
+
+df = pd.read_csv("/Users/tejes/Chrome-exten/PasswordManager/BackEnd/data.csv", on_bad_lines='skip')
+df=df.dropna()
+X=df.drop("strength",axis=1).values.flatten()
+y=df["strength"].values
+print("data processed")
+
+with open('/Users/tejes/Chrome-exten/PasswordManager/BackEnd/password_model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 Checker = Flask(__name__)
 
@@ -15,22 +31,10 @@ def welcome():
 
 @Checker.route('/model/', methods=['GET', 'POST'])
 def display():
-    def tokens(string):
-        """Break strings into characters."""
-        return [x for x in string]
-    
-    print("libraries imported")
-
-    df = pd.read_csv("/Users/tejes/Chrome-exten/PasswordManager/BackEnd/data.csv", on_bad_lines='skip')
-    df=df.dropna()
-    X=df.drop("strength",axis=1).values.flatten()
-    y=df["strength"].values
-    print("data processed")
-
-    with open('/Users/tejes/Chrome-exten/PasswordManager/BackEnd/password_model.pkl', 'rb') as file:
-        model = pickle.load(file)
-
-    print(model.score(X,y))
+    try:
+        return model.score(X,y)
+    except Exception as e :
+        return jsonify({"error": str(e)}),500
 
 if __name__ == '__main__':
     Checker.run(host='0.0.0.0', port=105)
